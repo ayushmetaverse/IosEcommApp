396c9d2f5cce42783a5ba9f47015aa27408a37f9
ayushmetaverse <ayushmt3@gmail.com>
2024-04-20 13:10:37 +0530

    firstCommit

---
+++ IosEcommApp/IosEcommAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosEcommAppApp.swift
+//  IosEcommApp
+//
+//  Created by apple on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct IosEcommAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
